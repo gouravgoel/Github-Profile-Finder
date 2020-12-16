7a5ef11d22ddd9f9ddbca7c1aee52b0ee9ccb602
gouravgoel <121gouravkolkata@gmail.com>
2020-12-16 23:32:20 +0530

    app ready to deploy

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.0.0</p>
+      <p>
+        Developed By:-{' '}
+        <a href='https://www.github.com/gouravgoel'>Gourav Goel</a>
+      </p>
+    </Fragment>
+  )
+}
+
+export default About
